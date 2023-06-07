2fdd95b43531d1ff3b95ab4d75c59d743a4e784c
Valeriyabogdanova <v.bogdanova2007@ya.ru>
2023-06-07 20:48:14 +0300

    функции аппер и тайтл

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def stroka(name):
+    """пишем имя заглавными буквами"""
     return name.upper()
+
+
+def stroka_2(name):
+    """функция возвращает первые буквы строки заглавными"""
+    return  name.title()
